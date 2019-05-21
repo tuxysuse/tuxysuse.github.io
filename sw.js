8711153ca80d39bf0cd48df4072928da6ce57653
Tiruveedhula, Aditya (at266g) <at266g@us.att.com>
2019-05-21 17:59:52 +0530

    Added push event listener in sw.js file

--- sw.js
+++ sw.js
@@ -43,3 +43,10 @@ self.addEventListener("fetch", event => {
     )
 
 });
+self.addEventListener('push', function(event) {
+    if (event.data) {
+      console.log('This push event has data: ', event.data.text());
+    } else {
+      console.log('This push event has no data.');
+    }
+  });
