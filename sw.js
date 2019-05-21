453fbf00fc9a80274a055153fad40ab5756cf316
Tiruveedhula, Aditya (at266g) <at266g@us.att.com>
2019-05-21 17:53:58 +0530

    Updated index.html to register a service worker file. Removed commented code from sw.js file.

--- sw.js
+++ sw.js
@@ -1,13 +1,3 @@
-// if('serviceWorker' in navigator) {
-//     navigator.serviceWorker.register('/sw.js')
-//     .then((registration) => {
-//         console.log("ServiceWorker registration succeeded.");
-//     })
-//     .catch((err) => {
-//         console.log('ServiceWorker registration failed: ', err);  
-//     })
-    
-// }
 const preCacheName = "pre-cache-hbp",
     preCacheFiles = [
         "/",
